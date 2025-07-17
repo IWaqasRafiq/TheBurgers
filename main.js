49c32e92870ec4a97c0afd30b609f4042f160603
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-07-17 04:51:05 -0700

    Complete TheBurgers Website

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const navMenu = document.getElementById("nav-menu");
 const navClose = document.getElementById("nav-close");
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(link => 
+navLink.forEach((link) =>
   link.addEventListener("click", () => {
     navMenu.classList.add("hidden");
   })
@@ -14,7 +14,7 @@ navClose.addEventListener("click", () => {
 });
 
 hamburger.addEventListener("click", () => {
-  navMenu.classList.remove('hidden');
+  navMenu.classList.remove("hidden");
 });
 
 const tabs = document.querySelectorAll(".tabs_warp ul li");
@@ -23,38 +23,86 @@ const foods = document.querySelectorAll(".food");
 const snacks = document.querySelectorAll(".snack");
 const beverages = document.querySelectorAll(".beverage");
 
-
-tabs.forEach(tab => {
-   tab.addEventListener("click", () => {
-    tabs.forEach(tab => {
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
       tab.classList.remove("active");
-    })
-    tab.classList.add('active');
-    
+    });
+    tab.classList.add("active");
+
     const tabval = tab.getAttribute("data-tabs");
 
-    all.forEach(item => {
-      item.style.display = 'none';
-    })
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
 
-    if (tabval == 'food') {
-      foods.forEach(item => {
-      item.style.display = 'block';
-    })
-    } else if (tabval == 'snack') {
-      snacks.forEach(item => {
-      item.style.display = 'block';
-    })
-    }
-    else if (tabval == 'beverage') {
-      beverages.forEach(item => {
-      item.style.display = 'block';
-    })
+    if (tabval == "food") {
+      foods.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "snack") {
+      snacks.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "beverage") {
+      beverages.forEach((item) => {
+        item.style.display = "block";
+      });
     } else {
-      all.forEach(item => {
-      item.style.display = 'block';
-    })
+      all.forEach((item) => {
+        item.style.display = "block";
+      });
     }
-
-   })
+  });
 });
+
+
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById("scroll-up");
+  if (this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2");
+    scrollUpBtn.classList.add("bottom-4");
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2");
+    scrollUpBtn.classList.remove("bottom-4");
+  }
+};
+
+window.addEventListener("scroll", scrollUp);
+
+
+const scrollHedaer = () => {
+  const header = document.getElementById("navbar");
+  if (this.scrollY >= 50) {
+    header.classList.add("border-b", "border-yellow-500");
+  } else {
+    header.classList.remove("border-b", "border-yellow-500");
+  }
+};
+
+window.addEventListener("scroll", scrollHedaer);
+
+const activeLink = () => {
+  const sections = document.querySelectorAll("section"); 
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  let current = "home";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+
+    if (this.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(item => {
+    item.classList.remove("text-secondaryColor");
+
+    if (item.href.includes(current)) {
+      item.classList.add("text-secondaryColor");
+    }
+  });
+}
+
+window.addEventListener("scroll", activeLink);
