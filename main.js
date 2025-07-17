acfbad2f55112704840b23c8f476602e7b125375
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-07-17 01:39:38 -0700

    some changing

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const hamburger = document.getElementById('hamburger');
+const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 const navClose = document.getElementById("nav-close");
 const navLink = document.querySelectorAll(".nav-link");
@@ -15,4 +15,46 @@ navClose.addEventListener("click", () => {
 
 hamburger.addEventListener("click", () => {
   navMenu.classList.remove('hidden');
+});
+
+const tabs = document.querySelectorAll(".tabs_warp ul li");
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const snacks = document.querySelectorAll(".snack");
+const beverages = document.querySelectorAll(".beverage");
+
+
+tabs.forEach(tab => {
+   tab.addEventListener("click", () => {
+    tabs.forEach(tab => {
+      tab.classList.remove("active");
+    })
+    tab.classList.add('active');
+    
+    const tabval = tab.getAttribute("data-tabs");
+
+    all.forEach(item => {
+      item.style.display = 'none';
+    })
+
+    if (tabval == 'food') {
+      foods.forEach(item => {
+      item.style.display = 'block';
+    })
+    } else if (tabval == 'snack') {
+      snacks.forEach(item => {
+      item.style.display = 'block';
+    })
+    }
+    else if (tabval == 'beverage') {
+      beverages.forEach(item => {
+      item.style.display = 'block';
+    })
+    } else {
+      all.forEach(item => {
+      item.style.display = 'block';
+    })
+    }
+
+   })
 });
